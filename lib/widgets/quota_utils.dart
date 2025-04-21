6a7ea442a7a211efef59910eeecb8c3a5921fe02
Hisoe <johnpadrilan402@gmail.com>
2025-04-21 21:19:16 +0800

    initial commit (used in get button)

---
+++ lib/widgets/quota_utils.dart
@@ -0,0 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+Future<int> getRemainingWeeklyQuantity() async {
+  final userId = FirebaseAuth.instance.currentUser!.uid;
+
+  final gets =
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(userId)
+          .collection('gets')
+          .where(
+            'timestamp',
+            isGreaterThan: DateTime.now().subtract(Duration(days: 7)),
+          )
+          .get();
+
+  int weeklyTotal = gets.docs.fold(
+    0,
+    (sum, doc) => sum + (doc.data()['quantity'] as int),
+  );
+
+  return 3 - weeklyTotal;
+}
